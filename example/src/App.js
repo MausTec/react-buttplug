f84f4e10d1d818787e610781e544d2c77f1b817f
Mau Abata <mauabata@gmail.com>
2021-02-07 23:34:18 -0600

    NO DIST

--- example/src/App.js
+++ example/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import ButtplugProvider from 'react-buttplug'
-import 'react-buttplug/dist/index.css'
 
 const App = () => {
   return <ButtplugProvider logLevel={"debug"} />
